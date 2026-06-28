e33e99728a49b582166628fd32522d98962ed827
Gobika404 <gobika404@gmail.com>
2026-06-28 07:57:53 +0530

    Update singleton.java

--- Week1/DesignPatterns/singleton.java
+++ Week1/DesignPatterns/singleton.java
@@ -1,1 +1,42 @@
+# Exercise 1 - Singleton Pattern
+
+class Logger {
+
+    private static Logger loggerObject;
+
+    private Logger() {
+        System.out.println("Logger Initialized");
+    }
+
+    public static Logger getLogger() {
+
+        if (loggerObject == null) {
+            loggerObject = new Logger();
+        }
+
+        return loggerObject;
+    }
+
+    public void writeLog(String message) {
+        System.out.println("Log : " + message);
+    }
+}
+
+public class SingletonPatternExample {
+
+    public static void main(String[] args) {
+
+        Logger firstLogger = Logger.getLogger();
+        Logger secondLogger = Logger.getLogger();
+
+        firstLogger.writeLog("Application Started");
+
+        if (firstLogger == secondLogger) {
+            System.out.println("Only one Logger object exists.");
+        } else {
+            System.out.println("Multiple Logger objects created.");
+        }
+
+    }
+}
 
