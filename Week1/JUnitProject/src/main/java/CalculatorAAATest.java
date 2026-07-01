1f1147651cebad82aca86490902b11dbabce6757
Gobika404 <gobika404@gmail.com>
2026-07-01 11:12:53 +0530

    Update CalculatorAAATest.java

--- Week1/JUnitProject/src/main/java/CalculatorAAATest.java
+++ Week1/JUnitProject/src/main/java/CalculatorAAATest.java
@@ -1,1 +1,42 @@
+import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorAAATest {
+
+    Calculator c;
+
+    @Before
+    public void setUp() {
+
+        c = new Calculator();
+
+        System.out.println("Before Test");
+
+    }
+
+    @After
+    public void tearDown() {
+
+        System.out.println("After Test");
+
+    }
+
+    @Test
+    public void testMultiply() {
+
+        // Arrange
+        int a = 5;
+        int b = 4;
+
+        // Act
+        int result = c.multiply(a,b);
+
+        // Assert
+        assertEquals(20,result);
+
+    }
+
+}
