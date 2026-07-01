1bcffdad39e1cacc51b68efab4d1b0f33671654f
Gobika404 <gobika404@gmail.com>
2026-07-01 11:07:20 +0530

    Update CalculatorTest.java

--- Week1/JUnitProject/src/main/test/java/CalculatorTest.java
+++ Week1/JUnitProject/src/main/test/java/CalculatorTest.java
@@ -1,1 +1,17 @@
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+
+        Calculator c = new Calculator();
+
+        int result = c.add(10,20);
+
+        assertEquals(30,result);
+    }
+
+}
