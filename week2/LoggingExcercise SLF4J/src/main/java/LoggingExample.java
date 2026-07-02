df0f7e85f21cee81d9be83d7e683582736d82b9e
Gobika404 <gobika404@gmail.com>
2026-07-02 05:32:23 +0530

    Update LoggingExample.java

--- week2/LoggingExcercise SLF4J/src/main/java/LoggingExample.java
+++ week2/LoggingExcercise SLF4J/src/main/java/LoggingExample.java
@@ -1,1 +1,30 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class LoggingExample {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingExample.class);
+
+    public static void main(String[] args) {
+
+        log.warn("Application execution has started.");
+
+        try {
+
+            int num1 = 20;
+            int num2 = 0;
+
+            int result = num1 / num2;
+
+            log.info("Result : {}", result);
+
+        } catch (ArithmeticException ex) {
+
+            log.error("Cannot divide a number by zero.", ex);
+
+        }
+
+        log.warn("Application execution completed.");
+
+    }
+}
